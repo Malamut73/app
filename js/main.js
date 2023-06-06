b3a7deeda2bc002020fec2168cd8a40d26440f65
Nikolay <lemoor@mail.ru>
2023-06-06 19:39:02 +0400

    top block

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function () {
+    $('.header__btn').on('click', function(){
+        $('.rightside__menu').removeClass('rightside__menu--close')
+    })
 
+    $('.rightside__menu-close').on('click', function(){
+        $('.rightside__menu').addClass('rightside__menu--close')
+    })
+
+    $('.top__slider').slick({
+        dots:true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
 })
