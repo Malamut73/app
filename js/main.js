a0dc2dfe6367b4cb6ce159354653c76cf5322761
Nikolay <lemoor@mail.ru>
2023-06-09 17:20:15 +0400

    page blog was done

--- js/main.js
+++ js/main.js
@@ -20,10 +20,20 @@ $(function () {
         dots:true,
         arrows: false,
     });
+    
+    $('.articale-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft">' + 
+                        '<img src="images/arrow-slide-left.svg">' + 
+                    '</button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright">' + 
+                        '<img src="images/arrow-slide-right.svg">' +
+                    '</button>'
+    });
 
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.living'
         }
     });
+
 })
